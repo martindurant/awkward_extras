6160bfafcbb625e5a90153c34135d70f2968bca4
Martin Durant <martin.durant@utoronto.ca>
2020-10-20 10:56:05 -0400

    accessor others

--- awkward_pandas/accessor.py
+++ awkward_pandas/accessor.py
@@ -4,6 +4,8 @@ import pandas as pd
 import awkward1 as ak
 from .series import AwkwardSeries
 
+funcs = [n for n in dir(ak) if inspect.isfunction(getattr(ak, n))]
+
 
 @pd.api.extensions.register_series_accessor("ak")
 class AwkwardAccessor:
@@ -27,14 +29,15 @@ class AwkwardAccessor:
     def __getattr__(self, item):
         from .series import AwkwardSeries
         # replace with concrete implementations of all top-level ak functions
-        funcs = [n for n in dir(ak) if inspect.isfunction(getattr(ak, n))]
         if item not in funcs:
             raise AttributeError
         func = getattr(ak, item)
 
         @functools.wraps(func)
         def f(*others, **kwargs):
-            ak_arr = func(self, *others, **kwargs)
+            others = [other.data if isinstance(getattr(other, "data", None), ak.Array) else other
+                      for other in others]
+            ak_arr = func(self._obj.data, *others, **kwargs)
             if isinstance(ak_arr, ak.Array):
                 return AwkwardSeries.from_awkward(ak_arr)
             return ak_arr
